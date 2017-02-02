4350240df349b592cb1cffd3f6df55ffc391a742
jatoroe <jatoroe@unal.edu.co>
2017-02-02 15:48:39 -0500

    Update

--- NetBeansProjects/BryanM/src/x.java
+++ NetBeansProjects/BryanM/src/x.java
@@ -1,10 +1,15 @@
 
 public class x {
     public static void main(String []arg){
-        System.out.println("hola mundo soy @lotalorafox");
+        System.out.println("hola mundo");
         
     }
 }
+public class j {
+  public static void main[ String [] jav){
+      System.out.println("Hola mundo soy javier");
+  }
+                          }
 
 
 
